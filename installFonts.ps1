af5cb5736791cd94e7605c853dd7d94ff0919363
n4t-dog <natc160@gmail.com>
2022-05-11 13:57:28 -0700

    Clean up font install

--- installFonts.ps1
+++ installFonts.ps1
@@ -1,15 +1,25 @@
-# WARNING! code is barely functional
+<#
+.SYNOPSIS
+  Installs the fonts "FiraCode Nerd" and "SourceSansPro" from web
+#>
 
+# Define list of zipped fonts
+$fontZips = @('https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip', 'https://www.fontsquirrel.com/fonts/download/source-sans-pro')
 
-# Download Fonts to Temp
-$fPath = Join-Path ([System.IO.Path]::GetTempPath()) 'fonts'
-$fZip = New-TemporaryFile
-Invoke-WebRequest 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip' -OutFile $fZip
-Expand-Archive $fZip $fPath
+# Define folder to extract to
+$fontPath = Join-Path ([System.IO.Path]::GetTempPath()) 'fonts'
+
+# Download and extract files
+foreach($fontZip in $fontZips){
+  $tempZip = New-TemporaryFile
+  Invoke-WebRequest $fontZip -OutFile $tempZip
+  Expand-Archive $tempZip $fontPath
+}
 
 # Invoke the system font installer
-$fonts = Get-ChildItem $fPath Fira*Complete.ttf
-foreach($f in ($fonts)){
-  Invoke-Item $f
+$fontFiles = Get-ChildItem $fontPath "Fira*Complete.ttf"
+$fontFiles+= Get-ChildItem $fontPath "SourceSansPro*.otf"
+foreach($fontFile in ($fontFiles)){
+  Invoke-Item $fontFile
   Pause
 }
